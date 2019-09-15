38d37d91e8bdadac32df6b5afd791fd518b3a26d
s3ptar <rene_graeber@graeber-elektronik.de>
2019-09-15 20:08:35 +0200

    Schematic added

--- Firmware/kwikstik_00/MK40DX256Z_INTERNAL_FLASH/Generated_Code/SegLCD1_c.d
+++ Firmware/kwikstik_00/MK40DX256Z_INTERNAL_FLASH/Generated_Code/SegLCD1_c.d
@@ -1,21 +1,21 @@
 Generated_Code/SegLCD1_c.obj: C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\SegLCD1.c \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewl_c_version.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\SegLCD1.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\PE_Types.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\stdint.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\ansi_parms.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewlGlobals.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\arm\ansi_prefix.ARM.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\os_enum.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\cstdint \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\limits_api.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\stdint.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\arm\ansi_prefix.ARM.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\cstdint \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ARM_EABI_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\PE_Error.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\PE_Const.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\IO_Map.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ProcessorExpert\lib\Kinetis\iofiles\MK40DZ10.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ProcessorExpert\lib\Kinetis\iofiles\MK40DZ10.h \
 	C:\projekte\Repo\KwikStick\Firmware\kwikstik_00\Generated_Code\Cpu.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ProcessorExpert\lib\Kinetis\pdd\inc\LCD_PDD.h \
-	C:\Freescale\CW\ MCU\ v11.0\MCU\ProcessorExpert\lib\Kinetis\pdd\inc\PDD_Types.h 
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ProcessorExpert\lib\Kinetis\pdd\inc\LCD_PDD.h \
+	C:\Freescale\CW\ MCU\ v11.1\MCU\ProcessorExpert\lib\Kinetis\pdd\inc\PDD_Types.h 
